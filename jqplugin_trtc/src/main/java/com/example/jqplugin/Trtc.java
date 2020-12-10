6ce0c34b42b5ed789509de87b38dfda95e3853c5
qiji <1204647949@qq.com>
2020-12-10 16:47:37 +0800

    123

--- jqplugin_trtc/src/main/java/com/example/jqplugin/Trtc.java
+++ jqplugin_trtc/src/main/java/com/example/jqplugin/Trtc.java
@@ -6,16 +6,10 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.alibaba.fastjson.JSONObject;
-//import com.tencent.imsdk.v2.V2TIMCallback;
-//import com.tencent.imsdk.v2.V2TIMManager;
-//import com.tencent.imsdk.v2.V2TIMSDKConfig;
-//import com.tencent.imsdk.v2.V2TIMSDKListener;
-
-import com.tencent.qcloud.tim.uikit.TUIKit;
-import com.tencent.qcloud.tim.uikit.base.IUIKitCallBack;
-import com.tencent.qcloud.tim.uikit.config.CustomFaceConfig;
-import com.tencent.qcloud.tim.uikit.config.GeneralConfig;
-import com.tencent.qcloud.tim.uikit.config.TUIKitConfigs;
+import com.tencent.imsdk.v2.V2TIMCallback;
+import com.tencent.imsdk.v2.V2TIMManager;
+import com.tencent.imsdk.v2.V2TIMSDKConfig;
+import com.tencent.imsdk.v2.V2TIMSDKListener;
 
 
 import io.dcloud.feature.uniapp.annotation.UniJSMethod;
@@ -24,24 +18,29 @@ import io.dcloud.feature.uniapp.common.UniModule;
 
 public class Trtc extends UniModule {
     final String TAG = "JQ-TRTC";
-    private UniJSCallback uniJSCallback;
-    private static Boolean loginStatus = false;
+    public UniJSCallback uniJSCallback = null;
 
     /**
      * IM登录
      * @param options
      * @param callback
      */
-    /*@UniJSMethod(uiThread = true)
+    @UniJSMethod(uiThread = true)
     public void config(final JSONObject options, UniJSCallback callback) {
-        uniJSCallback = callback;
         if(callback != null) {
+            uniJSCallback = callback;
             JSONObject data = new JSONObject();
-            data.put("code", "success");
+
             V2TIMSDKConfig config = new V2TIMSDKConfig();
             config.setLogLevel(V2TIMSDKConfig.V2TIM_LOG_INFO);
             Context context = mUniSDKInstance.getContext();
             int sdkAppID = (int) options.get("appid");
+
+//            int loginStatus = V2TIMManager.getInstance().getLoginStatus();
+//            if (loginStatus == V2TIMManager.V2TIM_STATUS_LOGINED || loginStatus == V2TIMManager.V2TIM_STATUS_LOGINING) {
+//                Log.e(TAG, "请不要重复登录");
+//                return;
+//            }
 
             V2TIMManager.getInstance().initSDK(context, sdkAppID, config, new V2TIMSDKListener() {
                 // 5. 监听 V2TIMSDKListener 回调
@@ -49,9 +48,6 @@ public class Trtc extends UniModule {
                 public void onConnecting() {
                     // 正在连接到腾讯云服务器
                     Log.e(TAG, "正在连接到腾讯云服务器");
-                    JSONObject data = new JSONObject();
-                    data.put("code", "正在登录");
-                    uniJSCallback.invokeAndKeepAlive(data);
                 }
                 @Override
                 public void onConnectSuccess() {
@@ -63,7 +59,6 @@ public class Trtc extends UniModule {
                         public void onError(int code, String desc) {
                             JSONObject data = new JSONObject();
                             data.put("code", "登录失败");
-                            Trtc.loginStatus = true;
                             uniJSCallback.invoke(data);
                         }
 
@@ -71,7 +66,6 @@ public class Trtc extends UniModule {
                         public void onSuccess() {
                             JSONObject data = new JSONObject();
                             data.put("code", "登录成功");
-                            Trtc.loginStatus = true;
                             uniJSCallback.invoke(data);
                         }
                     });
@@ -86,38 +80,8 @@ public class Trtc extends UniModule {
                 }
             });
         }
-    }*/
-
-    @UniJSMethod(uiThread = false)
-    public void config(final JSONObject options, UniJSCallback callback) {
-        uniJSCallback = callback;
-        if(callback != null) {
-            TUIKitConfigs configs = TUIKit.getConfigs();
-//            configs.setSdkConfig(new V2TIMSDKConfig());
-//            configs.setCustomFaceConfig(new CustomFaceConfig());
-//            configs.setGeneralConfig(new GeneralConfig());
-            Context context = mUniSDKInstance.getContext();
-            int sdkAppID = (int) options.get("appid");
-            TUIKit.init(context, sdkAppID, configs);
-            TUIKit.login((String) options.get("userId"), (String) options.get("userSig"), new IUIKitCallBack() {
-                @Override
-                public void onSuccess(Object data) {
-                    // 登录成功
-                    JSONObject result = new JSONObject();
-                    result.put("code", "登录成功");
-                    uniJSCallback.invoke(result);
-                }
-
-                @Override
-                public void onError(String module, final int code, final String desc) {
-                    // 登录失败
-                    JSONObject result = new JSONObject();
-                    result.put("code", "登录失败");
-                    uniJSCallback.invoke(result);
-                }
-            });
-        }
     }
+
     /**
      * 拨打电话
      * @param options
@@ -125,10 +89,15 @@ public class Trtc extends UniModule {
      */
     @UniJSMethod(uiThread = true)
     public void call(JSONObject options, UniJSCallback callback) {
-        Log.e(TAG, "testAsyncFunc--"+options);
-        if (!loginStatus) {
+        int loginStatus = V2TIMManager.getInstance().getLoginStatus();
+        if (loginStatus == V2TIMManager.V2TIM_STATUS_LOGOUT) {
             JSONObject data = new JSONObject();
             data.put("code", "未登录");
+            callback.invoke(data);
+            return;
+        } else if (loginStatus == V2TIMManager.V2TIM_STATUS_LOGINING) {
+            JSONObject data = new JSONObject();
+            data.put("code", "登录中");
             callback.invoke(data);
             return;
         }
